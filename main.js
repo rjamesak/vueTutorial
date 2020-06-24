9207c8327a7636b8df6df75e009b542178e7cd56
Russell James <jamesru@oregonstate.edu>
2020-06-23 17:19:09 -0800

    add computed properties

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
 var app = new Vue({  //new Vue instance plugs into an element in the DOM
     el: "#app", //html div has id "app"
     data: {
+        brand: "Vue Mastery",
         product: "Socks",
         description: "they warm your feet",
-        image: './assets/vmSocks-green.jpg',
-        link: 'http://www.adn.com',
-        inventory: 10,
+        //image: './assets/vmSocks-green.jpg',
+        selectedVariant: 0,
+        link: 'http://www.goldtoe.com',
+        //inventory: 10,
         onSale: true,
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
         variants: [
             {
                 variantId: 2234,
                 variantColor: "Green",
-                variantImage: './assets/vmSocks-green.jpg'
+                variantImage: './assets/vmSocks-green.jpg',
+                inventory: 10
             },
             {
                 variantId: 2235,
                 variantColor: "Blue",
-                variantImage: './assets/vmSocks-blue.jpg'
+                variantImage: './assets/vmSocks-blue.jpg',
+                inventory: 0
             }
         ],
         sizes: ["small", "medium", "large", "x-large"], 
@@ -26,16 +30,38 @@ var app = new Vue({  //new Vue instance plugs into an element in the DOM
     methods: {
         addToCart: function ()  {
             this.cart += 1;
-            this.inventory--;
+            this.variants[this.selectedVariant].inventory--;
+            this.toggleOnSale();
         },
-        updateProduct: function (variantImage) {
-            this.image = variantImage;
+        updateProduct: function (index) {
+            this.selectedVariant = index;
+            console.log(index);
+            console.log(this.variants[this.selectedVariant].variantImage);
         },
         removeFromCart() {
             if (this.cart > 0) {
                 this.cart -= 1;
-                this.inventory++;
+                this.variants[this.selectedVariant].inventory++;
+                this.toggleOnSale();
             }
+        },
+        toggleOnSale() {
+            if (this.variants[this.selectedVariant].inventory <= 2) {
+                this.onSale = false;
+            }
+            else {this.onSale = true;}
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + " " + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inventory() {
+            return this.variants[this.selectedVariant].inventory;
         }
     }
+
 })
