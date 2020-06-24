2e797306f0b50950e7ec682a63565ead4a1f743d
Russell James <jamesru@oregonstate.edu>
2020-06-24 11:53:31 -0800

    moved product html into vue component, updated html to use product tag

--- main.js
+++ main.js
@@ -1,6 +1,54 @@
-var app = new Vue({  //new Vue instance plugs into an element in the DOM
-    el: "#app", //html div has id "app"
-    data: {
+Vue.component('product', {
+    template: `
+        <div class="product">
+
+            <div class="product-image">
+                <img v-bind:src="image">
+            </div>
+
+            <div class="product-info">
+                <h1>{{title}}</h1>
+                <p>{{description}}</p>
+                <p v-show="inventory > 0">In Stock</p>
+                <p v-show="inventory == 0">Out of Stock</p>
+                <p :class="{struckThrough: !onSale}">On Sale!</p>
+                <p>Available: {{variants[selectedVariant].inventory}}</p>
+
+                <ul>
+                    <li v-for="detail in details">{{detail}}</li>
+                </ul>
+
+                <h4>Colors:</h4>
+                <div v-for="(variant, index) in variants" 
+                    :key="variant.variantId"
+                    class="color-box"
+                    :style="{ backgroundColor: variant.variantColor }"
+                    @mouseover="updateProduct(index), toggleOnSale()">
+                </div>
+
+                <h4>Available sizes:</h4>
+                <ul>
+                    <li v-for="size in sizes" :key="size">{{size}}</li>
+                </ul>
+
+                <button v-on:click="addToCart" 
+                        :disabled="!inventory"
+                        :class="{disabledButton: !inventory}">Add to Cart</button>
+                <button @click="removeFromCart">Remove from Cart</button>
+
+                <div class="cart">
+                    <p>Cart({{variants[selectedVariant].inCart}})</p>
+                </div>
+
+            </div>
+
+            <a :href="link">External link</a>
+
+            
+        </div> 
+    `,
+   data() {
+       return {
         brand: "Vue Mastery",
         product: "Socks",
         description: "they warm your feet",
@@ -15,21 +63,24 @@ var app = new Vue({  //new Vue instance plugs into an element in the DOM
                 variantId: 2234,
                 variantColor: "Green",
                 variantImage: './assets/vmSocks-green.jpg',
-                inventory: 10
+                inventory: 10,
+                inCart: 0
             },
             {
                 variantId: 2235,
                 variantColor: "Blue",
                 variantImage: './assets/vmSocks-blue.jpg',
-                inventory: 0
+                inventory: 4,
+                inCart: 0
             }
         ],
         sizes: ["small", "medium", "large", "x-large"], 
         cart: 0
-    }, 
+    }
+   }, //end data
     methods: {
         addToCart: function ()  {
-            this.cart += 1;
+            this.variants[this.selectedVariant].inCart += 1;
             this.variants[this.selectedVariant].inventory--;
             this.toggleOnSale();
         },
@@ -39,8 +90,8 @@ var app = new Vue({  //new Vue instance plugs into an element in the DOM
             console.log(this.variants[this.selectedVariant].variantImage);
         },
         removeFromCart() {
-            if (this.cart > 0) {
-                this.cart -= 1;
+            if (this.variants[this.selectedVariant].inCart > 0) {
+                this.variants[this.selectedVariant].inCart -= 1;
                 this.variants[this.selectedVariant].inventory++;
                 this.toggleOnSale();
             }
@@ -62,6 +113,10 @@ var app = new Vue({  //new Vue instance plugs into an element in the DOM
         inventory() {
             return this.variants[this.selectedVariant].inventory;
         }
-    }
+    } 
+})
+
+var app = new Vue({  //new Vue instance plugs into an element in the DOM
+    el: "#app", //html div has id "app"
 
 })
